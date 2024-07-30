0f5feb53bdfff91e55550afa8869012cdf2528bb
faustojuarez <70037264+FaustoJuarez@users.noreply.github.com>
2024-07-30 11:31:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-# Import required libraries
 import PIL
 import streamlit as st
 from ultralytics import YOLO
-import numpy as np
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
 
 # Replace the relative path to your weight file
 model_path = 'weights/best.pt'
 
 # Setting page layout
 st.set_page_config(
-    page_title="Deteccion de Rostros",
+    page_title="Deteccion de Insectos",
     layout="wide",
     initial_sidebar_state="expanded"
 )
@@ -22,7 +24,7 @@ with st.sidebar:
     confidence = 0.15
 
 # Creating main page heading
-st.title("Deteccion de Rostros con YOLOv8")
+st.title("Deteccion de Insectos con YOLOv8")
 
 # Creating two columns on the main page
 col1, col2 = st.columns(2)
@@ -37,26 +39,33 @@ except Exception as ex:
 # Adding image to the first column if image is uploaded
 if source_img:
     with col1:
-        # Opening the uploaded image
-        uploaded_image = PIL.Image.open(source_img)
-        # Adding the uploaded image to the page with a caption
-        st.image(uploaded_image,
-                 caption="Imagen cargada",
-                 use_column_width=True
-                 )
-    
-    if st.sidebar.button('Detectar insectos'):
-        # Convert PIL Image to numpy array
-        img_array = np.array(uploaded_image)
-        
-        # Perform prediction
-        res = model.predict(img_array, conf=confidence)
-        
-        boxes = res[0].boxes
-        res_plotted = res[0].plot()[:, :, ::-1]
-        
-        with col2:
-            st.image(res_plotted,
-                     caption='Imagen detectada',
+        try:
+            # Opening the uploaded image
+            uploaded_image = PIL.Image.open(source_img)
+            # Adding the uploaded image to the page with a caption
+            st.image(uploaded_image,
+                     caption="Imagen cargada",
                      use_column_width=True
                      )
+        except Exception as ex:
+            st.error("Error opening the image.")
+            st.error(ex)
+            logging.error(f"Error opening image: {ex}")
+    
+    if st.sidebar.button('Detectar insectos'):
+        try:
+            # Perform prediction
+            res = model(uploaded_image, conf=confidence)
+            
+            boxes = res[0].boxes
+            res_plotted = res[0].plot()[:, :, ::-1]
+            
+            with col2:
+                st.image(res_plotted,
+                         caption='Imagen detectada',
+                         use_column_width=True
+                         )
+        except Exception as ex:
+            st.error("Error during insect detection.")
+            st.error(ex)
+            logging.error(f"Error during detection: {ex}")
